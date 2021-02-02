ff5b852dfa9f9185ce92e2663b1f1941121e2021
Ben Glines <benglines1@gmail.com>
2021-02-01 19:27:10 -0700

    Updated to ready state

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,35 @@
 var txt = "Benjamin Glines";
 var speed = 100;
+var typeIte = 0;
 
-async function showDescription(descriptionID) {
+async function showDescription(descriptionID, experienceListItem) {
   var experiences = document.getElementsByClassName("experience__description");
+  var experienceListItems = document.getElementsByClassName("experience__list__item");
+
   for (i = 0; i < experiences.length; i++) {
-    experiences[i].style.opacity = 0; 
+    experiences[i].style.visibility = "hidden";
+    experiences[i].style.opacity = 0;
+    if(experienceListItems[i].style.opacity == "1") {
+      experienceListItems[i].style.opacity = "0.5";
+    } 
+    if(experienceListItems[i].style.boxShadow != "0px 0px 0px") {
+      experienceListItems[i].style.boxShadow = "0px 0px 0px";
+    }
   }
-  for (i = 0; i < experiences.length; i++) {
-    experiences[i].style.visibility = "hidden"; 
+
+  for (i = 0; i < experienceListItems.length; i++) {
+    if(experienceListItems[i].id == experienceListItem) {
+      experienceListItems[i].style.opacity = "1";
+      i += 1;
+      if(i < experienceListItems.length) {
+        experienceListItems[i].style.boxShadow = "inset 0 7px 9px -7px #00000080"
+      }
+      break;
+    }
   }
-  await sleep(300);
-  document.getElementById(descriptionID).style.opacity = 1;
   document.getElementById(descriptionID).style.visibility = "visible";
-  i = 0;
+  document.getElementById(descriptionID).style.opacity = 1;
+  // i = 0;
 }
 
 async function displayFront() {
@@ -33,9 +50,9 @@ async function scrollWiggle() {
 }
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("name").innerHTML += txt.charAt(i);
-    i++;
+  if (typeIte < txt.length) {
+    document.getElementById("name").innerHTML += txt.charAt(typeIte);
+    typeIte++;
     setTimeout(typeWriter, speed);
   }
 }
@@ -46,7 +63,7 @@ function sleep(ms) {
 
 window.addEventListener("load", function(){
   displayFront();
-  showDescription('cclDescription');
+  showDescription('cclDescription','cclItem');
 });
 
 var modal = document.getElementById("project__modal")
@@ -85,4 +102,14 @@ window.onclick = function(event) {
       modalContent.style.opacity = 0;
     }
   }
+}
+
+function displayProject(x) {
+  x.style.WebkitFilter = "grayscale(0%) brightness(1)";
+  x.style.filter = "grayscale(0%) brightness(1)";
+}
+
+function hideProject(x) {
+  x.style.WebkitFilter = "grayscale(50%) brightness(0.8)";
+  x.style.filter = "grayscale(50%) brightness(0.8)";
 }
